2e4f1e2b898665a80863aa26bd13414e737ce4d3
Mohammad Anas <anas4u02@gmail.com>
2022-07-19 13:13:04 +0530

    feat: authentication and redirection

--- articles/views.py
+++ articles/views.py
@@ -1,8 +1,37 @@
 import imp
+from django.http import QueryDict
 from django.shortcuts import render
 from articles.models import Article
 
 # Create your views here.
+def article_search_view(request):
+    print(request.GET)
+    query_dict = request.GET
+    # query = query_dict.get("query")
+    try:
+        query = int(query_dict.get("query"))
+    except:
+        query = None
+    article_obj = None
+    if query is not None:
+        article_obj = Article.objects.get(id=query)
+    context = {"object": article_obj}
+    return render(request, "articles/search.html", context=context)
+
+
+def article_create_view(request):
+    # print(request.POST)
+    context = {}
+    if request.method == "POST":
+        title = request.POST.get("title")
+        content = request.POST.get("content")
+        print(title, content)
+        article_object = Article.objects.create(title=title, content=content)
+        context["object"] = article_object
+        context["created"] = True
+    return render(request, "articles/create.html", context=context)
+
+
 def article_detail_view(request, id=None):
     article_obj = None
     if id is not None:
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -14,15 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
 from articles import views
 from .views import home_view
 
+from accounts.views import login_view, logout_view
+from articles.views import article_search_view, article_create_view, article_detail_view
+from .views import home_view
+
 urlpatterns = [
-    path(
-        "",
-        home_view,
-    ),  # index /home /root
-    path("articles/<int:id>/", views.article_detail_view),
+    path("", home_view),  # index / home / root
+    path("articles/", article_search_view),
+    path("articles/create/", article_create_view),
+    path("articles/<int:id>/", article_detail_view),
+    # re_path(r'articles/(?P<id>\d+)/$', home_view),
     path("admin/", admin.site.urls),
+    path("login/", login_view),
+    path("logout/", logout_view),
 ]
